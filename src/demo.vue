b2a52cd3b2bb77509fe6facc09b76e2e577682af
Just-do-it-LB <60215541+Just-do-it-LB@users.noreply.github.com>
2022-12-28 17:57:52 +0800

    1 file modified

--- src/demo.vue
+++ src/demo.vue
@@ -6,49 +6,22 @@
       :data="data"
       :columns="columns"
       :row-class-name="changeRowClass"
-      :rowAttributes="fff"
-    >
-      <template #footerSummary>
-        <div class="t-table__row-filter-inner">汇总：近期数据波动较大</div>
-      </template>
+      id="111"
+      :rowAttributes="[{ color: 'yellow' }]"
+      >å
     </t-table>
   </div>
 </template>
 
 <script lang="jsx">
-import {
-  ErrorCircleFilledIcon,
-  CheckCircleFilledIcon,
-  CloseCircleFilledIcon,
-} from 'tdesign-icons-vue';
-
 const data = [];
 const total = 5;
 for (let i = 0; i < total; i++) {
   data.push({
     index: i,
     applicant: ['贾明', '张三', '王芳'][i % 3],
-    status: i % 3,
-    channel: ['电子签署', '纸质签署', '纸质签署'][i % 3],
-    detail: {
-      email: ['w.cezkdudy@lhll.au', 'r.nmgw@peurezgn.sl', 'p.cumx@rampblpa.ru'][
-        i % 3
-      ],
-    },
-    matters: [
-      '宣传物料制作费用',
-      'algolia 服务报销',
-      '相关周边制作费',
-      '激励奖品快递费',
-    ][i % 4],
-    time: [2, 10, 1][i % 3],
-    createTime: [
-      '2022-01-01',
-      '2022-02-01',
-      '2022-03-01',
-      '2022-04-01',
-      '2022-05-01',
-    ][i % 4],
+    status: i,
+    color: ['#ff0011', '#ff0122', '#ff1033'][i % 3],
   });
 }
 export default {
@@ -61,86 +34,25 @@ export default {
           colKey: 'status',
           title: '审批状态',
           width: 120,
-          cell: (h, { row }) => {
-            const statusNameListMap = {
-              0: {
-                label: '审批通过',
-                theme: 'success',
-                icon: <CheckCircleFilledIcon />,
-              },
-              1: {
-                label: '审批失败',
-                theme: 'danger',
-                icon: <CloseCircleFilledIcon />,
-              },
-              2: {
-                label: '审批过期',
-                theme: 'warning',
-                icon: <ErrorCircleFilledIcon />,
-              },
-            };
-            return (
-              <t-tag
-                shape="round"
-                theme={statusNameListMap[row.status].theme}
-                variant="light-outline"
-              >
-                {statusNameListMap[row.status].icon}
-                {statusNameListMap[row.status].label}
-              </t-tag>
-            );
-          },
         },
         {
-          colKey: 'time',
-          title: '申请耗时(天)',
+          colKey: 'color',
+          title: '行颜色',
           width: 120,
-          align: 'center',
-          // 设置单元格类名
-          className: ({ row }) => {
-            if (row.time >= 9) {
-              return 'custom-cell-class-name';
-            }
-            if (row.time == 2) {
-              return 'custom-cell-class-name22';
-            }
-            return '';
-          },
-          attrs: ({ row }) => {
-            if (row.time >= 9) {
-              return {
-                style: {
-                  fontWeight: 600,
-                  backgroundColor: 'var(--td-warning-color-light)',
-                },
-              };
-            }
-          },
         },
-        {
-          colKey: 'channel',
-          title: '签署方式',
-          width: 120,
-          align: 'right',
-          className: () => 'custom-cell-class-name',
-        },
-        {
-          colKey: 'detail.email',
-          title: '邮箱地址',
-          width: 160,
-          ellipsis: true,
-        },
-        { colKey: 'createTime', title: '申请时间' },
       ],
     };
   },
 
   methods: {
     changeRowClass(params) {
-      if (params.rowIndex === 2) return 'custom-third-class-name';
+      // let style = document.createElement('style');
+      // style.backgroundColor = 'yellow';
+      // document.getElementsByTagName('head')[0].appendChild(style);
+
+      if (params.rowIndex >= 1) return 'custom-third-class-name';
       return 'custom-third-classall';
     },
-    fff() {},
   },
 };
 </script>
@@ -154,7 +66,7 @@ export default {
 .t-demo__style .t-table .custom-third-classall > td {
   color: green;
   font-weight: bold;
-  background-color: blue;
+  background-color: pink;
 }
 
 .t-demo__style .t-table td.last-column-class-name {
